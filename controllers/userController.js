8a2d1f68f8b7ecc4ba626cc8c3a9f84d9266d19f
Jamie Otto <jmotto31@gmail.com>
2022-10-05 15:18:25 -0700

    user controller add friend, remove friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,8 @@ module.exports = {
   },
   //  GET "/:userId"
   // User.findById( req.params.userId )
-// ---> another way to find one user User.findOne ( { _id: req.params.userId})
-// .populate('field that has reference id');
+  // ---> another way to find one user User.findOne ( { _id: req.params.userId})
+  // .populate('field that has reference id');
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .populate("Thought")
@@ -38,14 +38,32 @@ module.exports = {
       .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
-
-
+  // PUT ""/:userId""
+  // User.findOneAndUpdate( search, { $set: req.body })
+  updateUser(req, res) {
+    User.findOneAndUpdate( search, { $set: req.body })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  //  add friend
+  addFriend( req, res ) {
+    console.log('You are adding an assignment');
+    console.log(req.body);
+    User.findOneAndUpdate(
+        { _id: req.params.userId},
+        { $addToSet: { friends: req.body} },
+    )
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  removeFriend( req, res ) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId},
+        { $pull: { friend: {friendID: req.params.friendID} } },
+    )
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  }
 };
 
 
-
-
-
-// PUT ""/:userId""
-
-// User.findOneAndUpdate( search, { $set: req.body })
